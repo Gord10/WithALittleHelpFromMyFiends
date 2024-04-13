0d8ec9012ce85e972a580120b78d9d863dde1b90
Gord10 <akk13us@yahoo.com>
2024-04-13 21:50:37 +0300

    DOTween, XP bar fills with tween

--- Assets/Scripts/UI/GameUi.cs
+++ Assets/Scripts/UI/GameUi.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameUi : MonoBehaviour
 {
     public Image hpBar;
     public Image xpBar;
-
+    public float fillTweenSpeed = 1;
     public void SetHpBar(float hp, float maxHp)
     {
-        hpBar.fillAmount = hp / maxHp;
+        float fillAmount = hp / maxHp;
+        hpBar.fillAmount = fillAmount;
     }
 
     public void SetXpBar(int crystalsNum, int requiredCrystalsNum)
     {
-        xpBar.fillAmount = (float)crystalsNum / (float)requiredCrystalsNum;
+        float fillAmount = (float)crystalsNum / (float)requiredCrystalsNum;
+        xpBar.DOKill();
+
+        if(fillAmount == 0)
+        {
+            xpBar.fillAmount = 0;
+        }
+        else
+        {
+            xpBar.DOFillAmount(fillAmount, fillTweenSpeed).SetSpeedBased();
+        }
     }
 }
